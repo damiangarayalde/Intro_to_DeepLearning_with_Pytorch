fbc3d53015ee5cf0474ee1e5a5f564473110ae77
damiangarayalde <damiangarayalde@gmail.com>
2022-08-13 18:42:57 -0300

    Part 4

--- intro-to-pytorch/helper.py
+++ intro-to-pytorch/helper.py
@@ -70,8 +70,10 @@ def view_classify(img, ps, version="MNIST"):
     ps = ps.data.numpy().squeeze()
 
     fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
+
     ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
     ax1.axis('off')
+    
     ax2.barh(np.arange(10), ps)
     ax2.set_aspect(0.1)
     ax2.set_yticks(np.arange(10))
